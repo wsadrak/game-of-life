ece19993c0457955a92fd47b99b8434c4ba6edc2
wsadrak <w.sadrak@gmail.com>
2019-05-30 12:46:21 +0200

    removed toString() method

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,21 +1,15 @@
 package model;
 
-
 public enum Cell {
-	DEAD(" "), ALIVE("X");
-	
+	DEAD(" "), ALIVE("o");
 	private String symbol;
+
+	public String getSymbol() {
+		return symbol;
+	}
 
 	Cell(String symbol) {
 		this.symbol = symbol;
 	}
 
-	@Override
-	public String toString() {
-		return symbol;
-	}
-	
-
-	
-	
 }
