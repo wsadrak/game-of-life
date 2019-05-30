b573fe9bfcbe73e169addad436a91211f7d7f430
wsadrak <w.sadrak@gmail.com>
2019-05-30 12:47:30 +0200

    added a new method for printing cell characters

--- src/io/ConsolePrinter.java
+++ src/io/ConsolePrinter.java
@@ -1,7 +1,13 @@
 package io;
 
+import model.Cell;
+
 public class ConsolePrinter {
 	public void print(String message) {
 		System.out.println(message);
+	}
+	
+	public void printCell(Cell cell) {
+		System.out.print(cell.getSymbol() + " ");
 	}
 }
