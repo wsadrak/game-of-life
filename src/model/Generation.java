d6da9f0e2411b5f73036a4801c3c4cb5c36731f1
wsadrak <w.sadrak@gmail.com>
2019-05-30 12:52:15 +0200

    methods refactoring

--- src/model/Generation.java
+++ src/model/Generation.java
@@ -4,144 +4,108 @@ import java.util.Random;
 
 import javax.swing.CellRendererPane;
 
-public class Board {
-//	private int rows;
-//	private int cols;
-	private final static int BOARD_HEIGHT = 25;
-	private Cell[][] plansza;
+import io.ConsolePrinter;
 
-	public Board() {
-		plansza = new Cell[BOARD_HEIGHT][BOARD_HEIGHT];
+public class Generation {
+	ConsolePrinter printer = new ConsolePrinter();
+	private Cell[][] board;
+	private int rows;
+	private int columns;
+
+	public Generation(int height, int width) {
+		rows = height;
+		columns = width;
+		board = new Cell[rows][columns];
+		filfullBoard();
 	}
 
-	public void createFirstGeneration() {
+	private void filfullBoard() {
+		Random random = new Random();
+		boolean isAlive = false;
 
-		for (int i = 0; i < BOARD_HEIGHT; i++) {
-			for (int j = 0; j < BOARD_HEIGHT; j++) {
-				Random rand = new Random();
-				boolean value = rand.nextBoolean();
-				if (value) {
-					plansza[i][j] = Cell.ALIVE;
+		for (int currentRow = 0; currentRow < rows; currentRow++) {
+			for (int currentColumn = 0; currentColumn < columns; currentColumn++) {
+				isAlive = random.nextBoolean();
+				if (isAlive) {
+					board[currentRow][currentColumn] = Cell.ALIVE;
 				} else {
-					plansza[i][j] = Cell.DEAD;
+					board[currentRow][currentColumn] = Cell.DEAD;
 				}
 			}
 		}
 	}
 
-
 	public void print() {
-		for (int i = 0; i < BOARD_HEIGHT; i++) {
-			for (int j = 0; j < BOARD_HEIGHT; j++) {
-				System.out.print(plansza[i][j] + " ");
+		for (int currentRow = 0; currentRow < rows; currentRow++) {
+			for (int currentColumn = 0; currentColumn < columns; currentColumn++) {
+				printer.printCell(board[currentRow][currentColumn]);
 			}
-			System.out.println();
+			printer.print("\n");
 		}
 	}
 
 	public void update() {
-//		for (int i = 0; i < BOARD_HEIGHT; i++) {
-//			for (int j = 0; j < BOARD_HEIGHT; j++) {
-//
-//				bornOrDie(plansza[i][j]);
-//			}
-//		}
-		
-		for(int i = 0; i < BOARD_HEIGHT; i++) {
-			for(int j =  0; j < BOARD_HEIGHT; j++) {
-				int howMany = neighboursCountAt(i,j);
-				
-				if(howMany == 3 || (howMany == 2 && plansza[i][j] == Cell.ALIVE)) {
-					plansza[i][j] = Cell.ALIVE;
+		for (int currentRow = 0; currentRow < rows; currentRow++) {
+			for (int currentColumn = 0; currentColumn < columns; currentColumn++) {
+				int neighbours = neighboursCountAt(currentRow, currentColumn);
+				if (neighbours == 3 || (neighbours == 2 && board[currentRow][currentColumn] == Cell.ALIVE)) {
+					board[currentRow][currentColumn] = Cell.ALIVE;
 				} else {
-					plansza[i][j] = Cell.DEAD;
+					board[currentRow][currentColumn] = Cell.DEAD;
 				}
 			}
 		}
-		
 	}
 
-	
-	private int neighboursCountAt(int row, int col) {
-		int sum = 0;
-		
-		  if (row != 0 && col != 0){    //1
-	            if(plansza[row-1][ col-1] == Cell.ALIVE) {
-	            	sum++;
-	            }
-	        }
-	        
-	        if (row != 0){
-//	            if(isAlive(row-1,col)){ //2
-	            	if(plansza[row-1][ col] == Cell.ALIVE) {
-	            sum++;
-	            }
-	        }
-	        
-	        if (row != 0 && col != BOARD_HEIGHT-1){//3
-//	            if(isAlive(row-1,col+1)){
-	            	if(plansza[row-1][ col+1] == Cell.ALIVE) {
-	                sum++;
-	            }
-	        }
-	        if (col != 0){
-//	            if(isAlive(row,col-1)){ //4
-	            	if(plansza[row][ col-1] == Cell.ALIVE) {
-	            sum++;
-	            }
-	        }
-	        //self
-	        if (col != BOARD_HEIGHT-1){
-//	            if(isAlive(row,col+1)){ //6
-	        	if(plansza[row][ col+1] == Cell.ALIVE) {
-	                sum++;
-	            }
-	        }
+	private int neighboursCountAt(int currentRow, int currentColumn) {
+		int neighbours = 0;
 
-	        if (row != BOARD_HEIGHT-1 && col != 0){
-	        	if(plansza[row+1][ col-1] == Cell.ALIVE) {
-//	            if(isAlive(row+1,col-1)){ //7
-	                sum++;
-	            }
-	        }
-
-	        if (row != BOARD_HEIGHT-1){
-//	            if(isAlive(row+1,col)){ //8
-	            	if(plansza[row+1][ col] == Cell.ALIVE) {
-	            sum++;
-	            }
-	        }
-
-	        if (row != BOARD_HEIGHT-1 && col != BOARD_HEIGHT-1){
-//	            if(isAlive(row+1,col+1)){ //9
-	        	if(plansza[row+1][ col+1] == Cell.ALIVE) {
-	                sum++;
-	            }
-	        }
-
-	        return sum;
-		
-	}
-
-
-	private void bornOrDie(Cell singleCell) throws ArrayIndexOutOfBoundsException {
-		for (int i = 0; i < BOARD_HEIGHT; i++) {
-			for (int j = 0; j < BOARD_HEIGHT; j++) {
-
-//				int neigbours = calculateNeigbours();
-//				int neigbours = 0;
-//
-//				if (singleCell.isValue()) {
-//					if (neigbours < 2 || neigbours > 3) {
-//						singleCell.setValue(false);
-//					}
-//				} else {
-//					if (neigbours == 3) {
-//						singleCell.setValue(true);
-//					}
-//				}
+		if (currentRow != 0 && currentColumn != 0) { // 1
+			if (board[currentRow - 1][currentColumn - 1] == Cell.ALIVE) {
+				neighbours++;
 			}
 		}
+
+		if (currentRow != 0) {
+			if (board[currentRow - 1][currentColumn] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+
+		if (currentRow != 0 && currentColumn != columns - 1) {// 3
+			if (board[currentRow - 1][currentColumn + 1] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+		if (currentColumn != 0) {
+			if (board[currentRow][currentColumn - 1] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+		if (currentColumn != columns - 1) {
+			if (board[currentRow][currentColumn + 1] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+
+		if (currentRow != rows - 1 && currentColumn != 0) {
+			if (board[currentRow + 1][currentColumn - 1] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+
+		if (currentRow != rows - 1) {
+			if (board[currentRow + 1][currentColumn] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+
+		if (currentRow != rows - 1 && currentColumn != columns - 1) {
+			if (board[currentRow + 1][currentColumn + 1] == Cell.ALIVE) {
+				neighbours++;
+			}
+		}
+		return neighbours;
 	}
 
 }
