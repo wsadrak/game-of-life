c93713901f2827ea5af5a7bcecad4834b0c038b6
wsadrak <w.sadrak@gmail.com>
2019-05-30 12:48:58 +0200

    refactoring, error handling has been moved to another class

--- src/app/Application.java
+++ src/app/Application.java
@@ -4,12 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		GameController controller = new GameController();
-		try {
-			controller.mainLoop();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		controller.mainLoop();
 	}
 
 }
